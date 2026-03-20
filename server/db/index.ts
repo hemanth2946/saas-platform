5aaf2279da347fa43e390819b92cf30b4a9bb809
hemanth2946 <hemanthkumar2946@gmail.com>
2026-03-20 14:57:07 +0530

    fix: Prisma 7 client import path

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../generated/prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
 const globalForPrisma = globalThis as unknown as {
@@ -12,8 +12,7 @@ function createPrismaClient() {
     return new PrismaClient({ adapter });
 }
 
-export const prisma =
-    globalForPrisma.prisma ?? createPrismaClient();
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
